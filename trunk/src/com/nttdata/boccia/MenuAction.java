65e0fea22a37352291ebd2ac28aea33371be73fa
mondovich@gmail.com <mondovich@gmail.com@01f8c7a4-ffc0-8f4e-1c6f-d777049d1c85>
2013-10-16 13:06:34 +0000

    Funzionante, con log4j!

--- trunk/src/com/nttdata/boccia/MenuAction.java
+++ trunk/src/com/nttdata/boccia/MenuAction.java
@@ -1,8 +1,10 @@
 package com.nttdata.boccia;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.Query;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,7 +22,8 @@ import com.nttdata.boccia.em.EMF;
 
 public class MenuAction extends Action {
 	
-	private static Log log = LogFactory.getLog(MenuAction.class);
+	private static Log log = LogFactory.getLog(Menu.class);
+//	private static Logger logger = Logger.getLogger(MenuAction.class.getName());
 	
 	EntityManager em = EMF.get().createEntityManager();
 
@@ -34,10 +37,28 @@ public class MenuAction extends Action {
 		Query q = em.createQuery("select from Menu m where m.date = :date");
 		q.setParameter("date", new Date());
 
-		Menu menu = (Menu) q.getSingleResult();
+		log.info("MENU!");
 		
+		Menu menu = null;
+		try {
+			menu = (Menu) q.getSingleResult();
+			
+			log.info(menu.toString());
+		} catch (NoResultException e) {
+			log.info("No menu found.");
+			
+			menu = new Menu();
+			menu.setDate(new Date());
+			
+			ArrayList<String> primi = new ArrayList<String>();
+			primi.add("Pasta");
+			menu.setPrimi(primi);
+			
+			em.persist(menu);
+		} 
+		
+		request.setAttribute("ciao", "Ciao");
 		request.setAttribute("menu", menu);
-		if (log.isDebugEnabled()) log.debug(menu); 
 		
 		return forward;
 	}
